ab5a95f346884d3625fdd606054b2654c267c4a4
itwithlyam <64089164+itwithlyam@users.noreply.github.com>
2021-04-07 20:39:01 +0100

    [u] adding headings

--- after.php
+++ after.php
@@ -6,3 +6,17 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
 }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome</title>
+    </head>
+    
+    <body>
+        
+        <h1>Logged in as <?php echo $_SESSION["username"]; ?>!</h1>
+        
+    </body>
+</html>
+    
