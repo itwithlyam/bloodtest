4531bbb694a53845e2d3293c0e0cc1f3bf6b8bc0
itwithlyam <64089164+itwithlyam@users.noreply.github.com>
2021-04-07 20:25:07 +0100

    [a] login system

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'sql4.freesqldatabase.com');
+define('DB_USERNAME', 'sql4403042');
+define('DB_PASSWORD', 'fqR7RmULDW');
+define('DB_NAME', 'sql4403042');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+  <body>
+  <h1>IT with Lyam</h1>
+  <a href="welcome.php">Go to app</a>
+  </body>
+</html>
